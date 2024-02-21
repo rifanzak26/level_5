f972e3497eccec9b30ea85356280081eaaf5d816
rifanzak26 <rifanzamuhammad@gmail.com>
2024-02-21 13:23:55 +0700

    second commit:added basicAuth

--- main.go
+++ main.go
@@ -5,12 +5,35 @@ import (
 	"level_5/controller"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 )
 
 func main() {
 	app := fiber.New()
 
 	config.Connect()
+
+	app.Use(basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			"john":  "doe",
+			"admin": "123456",
+		},
+		Realm: "Forbidden",
+		Authorizer: func(user, pass string) bool {
+			if user == "john" && pass == "doe" {
+				return true
+			}
+			if user == "admin" && pass == "123456" {
+				return true
+			}
+			return false
+		},
+		Unauthorized: func(c *fiber.Ctx) error {
+			return c.Status(401).SendString("Unauthorized")
+		},
+		ContextUsername: "_user",
+		ContextPassword: "_pass",
+	}))
 
 	app.Get("/department", controller.GetDepartment)
 	app.Post("/department", controller.AddDepartment)
